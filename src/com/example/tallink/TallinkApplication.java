99743f7503cf77ab8de3744ad84cdf712f7cef5d
SK <lunaticman@gmail.com>
2012-07-27 12:53:12 +0300

    first layout version completed

--- src/com/example/tallink/TallinkApplication.java
+++ src/com/example/tallink/TallinkApplication.java
@@ -1,15 +1,34 @@
 package com.example.tallink;
 
 import com.vaadin.Application;
-import com.vaadin.ui.*;
+import com.vaadin.ui.Window;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Table;
 
 public class TallinkApplication extends Application {
+	
+	private Button button = new Button("Populate Table");
+	private Window mainWindow = new Window("Tallink Application");
+	private Table table = new Table();
+	
 	@Override
 	public void init() {
-		Window mainWindow = new Window("Tallink Application");
-		Label label = new Label("Hello Vaadin user");
-		mainWindow.addComponent(label);
+		initLayout();
+		initTable();
+	}
+	
+	private void initTable() {
+		table.addContainerProperty("something", String.class, "defaultValue");
+		table.addContainerProperty("another column", String.class, "defaultValue");
+		table.setSelectable(true);
+	}
+
+	public void initLayout(){
+		mainWindow.addComponent(button);
+		mainWindow.addComponent(table);
 		setMainWindow(mainWindow);
 	}
+	
+	
 
 }
