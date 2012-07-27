bf27a40a13ca0ac9a5068b7d9a2da728b25327ea
SK <lunaticman@gmail.com>
2012-07-27 12:52:35 +0300

    create database script

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,1 +1,15 @@
-test
+CREATE DATABASE tallink;
+
+GRANT ALL PRIVILEGES
+ON tallink.*
+TO 'tallink_user'@'localhost'
+IDENTIFIED BY 'tallink_pass'
+WITH GRANT OPTION;
+
+GRANT ALL PRIVILEGES
+ON tallink.*
+TO 'tallink'@'%'
+IDENTIFIED BY 'tallink_pass'
+WITH GRANT OPTION;
+
+USE tallink;
